ca3f33fdd37151c1aa67a07494e3f8305460c94c
Ati Vassileva <ativassileva@gmail.com>
2021-12-02 15:51:50 +0200

    Introduced diets catalog

--- src/components/Catalogs/Diets/Diet.js
+++ src/components/Catalogs/Diets/Diet.js
@@ -1,24 +1,22 @@
-const Diet = ({
-    imageClasses,
-    textClasses,
-    imageUrl,
-    heading,
-    description,
-}) => (
-    <div>
-        <div class={imageClasses}>
-            <div class="ss-pic">
-                <img src={imageUrl} alt="" />
-            </div>
-        </div>
-        <div class={textClasses}>
-            <div class="ss-text">
-                <h4>{heading}</h4>
-                <p>{description}</p>
-                <a href="">Explore</a>
-            </div>
-        </div>
-    </div>
-)
+import { Link } from 'react-router-dom';
 
+const Diet = ({
+    diet
+}) => {
+    return (
+        <div>
+            <div className="blog-item">
+                <div className="bi-pic">
+                    <img src={diet.imageUrl} alt="" />
+                </div>
+                <div className="bi-text">
+                    <h5><Link to={`/details/${diet.id}`}>{diet.name}</Link></h5>
+                    <p>{diet.description.substring(0, 500) + '... '}
+                    <Link to={`/details/${diet.id}`}>Read more...</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
 export default Diet;
--- src/services/dietService.js
+++ src/services/dietService.js
@@ -1,5 +1,17 @@
-import { collection, addDoc } from "@firebase/firestore";
+import { onSnapshot, collection, addDoc } from "@firebase/firestore";
 import db from '../firebase';
+
+export const getAllDiets = () => {
+    let diets = [];
+
+    onSnapshot(collection(db, 'diets'),
+        (snapshot) => snapshot.docs.forEach((doc) => {
+            let diet = { ...doc.data(), id: doc.id };
+            diets.unshift(diet);
+        }));
+
+    return diets;
+}
 
 export const createNewDiet = async (name, imageUrl, description) => {
     const collectionRef = collection(db, "diets");
