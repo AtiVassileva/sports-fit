913430bb52ff7738cad0ecf9e316d5c77544a07c
Ati Vassileva <ativassileva@gmail.com>
2021-12-13 22:02:50 +0200

    Introduced blog pagination

--- src/components/Catalogs/Blog/Main.js
+++ src/components/Catalogs/Blog/Main.js
@@ -1,15 +1,30 @@
 import Post from "./Post";
-import Pagination from "../Pagination";
+import ReactPaginate from 'react-paginate';
 import { useState, useEffect } from "react";
 import * as blogService from "../../../services/blogService";
 
 const Main = () => {
 
     const [articles, setArticles] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const articlesPerPage = 2;
+    const pagesVisited = pageNumber * articlesPerPage;
+
+    const displayArticles =
+        articles
+            .slice(pagesVisited, pagesVisited + articlesPerPage)
+            .map(x => <Post key={x.id} article={x} />);
+
+    const pageCount = Math.ceil(articles.length / articlesPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    }
 
     useEffect(() => {
         blogService.getAllArticles()
-        .then(articles => setArticles(articles));
+            .then(articles => setArticles(articles));
     }, []);
 
     return (
@@ -17,15 +32,15 @@ const Main = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12 p-0">
-                        {articles
-                            .map(x =>
-                                <Post
-                                    key={x.id}
-                                    article={x}
-                                />
-                            )
-                        }
-                        <Pagination appSection="blog" />
+                        {displayArticles}
+                        <ReactPaginate
+                            previousLabel={"Previous"}
+                            nextLabel={"Next"}
+                            pageCount={pageCount}
+                            onPageChange={changePage}
+                            containerClassName={"blog-pagination"}
+                            disabledClassName={"disabled-btn"}
+                        />
                     </div>
                 </div>
             </div>
